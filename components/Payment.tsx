bd6d1fb2498776915795798ffb83144566d862c4
jasielochoa <jasieloc@gmail.com>
2025-12-16 08:43:47 -0600

    feat: Integrate Stripe for payment processing by adding dependencies, wrapping the booking flow with `StripeProvider`, and implementing payment sheet logic.

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,7 +1,49 @@
+import {
+  IntentCreationCallbackParams,
+  useStripe,
+} from '@stripe/stripe-react-native';
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import CustomButton from './CustomButton';
 
 const Payment = () => {
-  const openPaymentSheet = () => {};
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [success, setSuccess] = useState(false);
+
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: 'Example, Inc.',
+      intentConfiguration: {
+        mode: {
+          amount: 1099,
+          currencyCode: 'USD',
+        },
+        confirmHandler: confirmHandler,
+      },
+    });
+    if (error) {
+      // handle error
+    }
+  };
+
+  const confirmHandler = async (
+    confirmationToken,
+    intentCreationCallback: (params: IntentCreationCallbackParams) => void
+  ) => {
+    // explained later
+  };
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      setSuccess(true);
+    }
+  };
+
   return (
     <>
       <CustomButton
